4815237297779bf8f5eaac047747de2c2fbf835e
DASitby <davidsooley551@gmail.com>
2022-12-17 14:51:05 +0000

    remove console log

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,6 @@ const request = require('request');
 const fetchBreedDescription = function(query, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`,(error, response, body) => {
     if (error) {
-      console.log('Error:' + error);
       callback(error,null);
     }
     const data = JSON.parse(body);
